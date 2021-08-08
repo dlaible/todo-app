d15eae1ff64dbbdf39bf68e5be41efcf91007a4a
dlaible <dlaible7@gmail.com>
2021-08-07 21:27:24 -0700

    Removed unused files, added initial app skeleton

---
+++ README.md
@@ -0,0 +1,9 @@
+# TODO App
+
+> A simple yet pretty, non-persistent TODO app I made to occupy some time on a lazy Saturday afternoon.
+
+## Project Init
+
+```
+npm init @vitejs/app --template react-ts
+```
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+import './main.css'
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      Hello World!
+    </React.StrictMode>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
